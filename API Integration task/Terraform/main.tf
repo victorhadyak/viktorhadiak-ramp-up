5359835e0f5b917646d6061d98cbb782e6dc23d6
Viktor <109483154+victorhadyak@users.noreply.github.com>
2023-03-09 16:44:04 +0100

    Update main.tf

--- API Integration task/Terraform/main.tf
+++ API Integration task/Terraform/main.tf
@@ -1,11 +1,1 @@
-# Define the Terraform module
-module "my_lambda_function" {
-  source = "git::https://github.com/victorhadyak/viktorhadiak-ramp-up/blob/main/API%20Integration%20task/Terraform"
-  
-  # Function configuration
-  function_name     = "my-lambda-function"
-  handler           = "lambda_function.lambda_handler"
-  runtime           = "python3.9"
-  source_code_hash  = "filebase64sha256("${path.module}/my-lambda-function.zip")"
-  timeout           = 60
-  memory_size       = 128
+
