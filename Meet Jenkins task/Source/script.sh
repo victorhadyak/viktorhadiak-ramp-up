47613dce51fe15c4754e36c2843fdb2fdd738bc2
Viktor <109483154+victorhadyak@users.noreply.github.com>
2023-03-10 17:08:17 +0100

    Update script.sh

--- Meet Jenkins task/Source/script.sh
+++ Meet Jenkins task/Source/script.sh
@@ -1,1 +1,21 @@
-
+#!/bin/bash
+#Installing Jenkins, Git and Terraform
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install fontconfig openjdk-11-jre -y
+sudo apt-get install jenkins -y
+sudo systemctl start jenkins
+sudo apt-get install git -y
+sudo apt-get install gpg
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+    gpg --dearmor | \
+    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+    sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update -y
+sudo apt install terraform -y
